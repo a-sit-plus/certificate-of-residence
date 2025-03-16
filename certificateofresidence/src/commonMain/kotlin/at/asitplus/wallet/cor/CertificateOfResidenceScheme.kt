392b11c88ca4556c16c75ea7b831149b3ba24c82
Christian Kollmann <christian.kollmann@a-sit.at>
2025-03-16 12:47:33 +0100

    Fix wrong sdJwtType

--- certificateofresidence/src/commonMain/kotlin/at/asitplus/wallet/cor/CertificateOfResidenceScheme.kt
+++ certificateofresidence/src/commonMain/kotlin/at/asitplus/wallet/cor/CertificateOfResidenceScheme.kt
@@ -7,7 +7,9 @@ import at.asitplus.wallet.lib.data.ConstantIndex.CredentialScheme
 object CertificateOfResidenceScheme : CredentialScheme {
 
     override val schemaUri: String = "https://wallet.a-sit.at/schemas/1.0.0/cor.json"
-    override val sdJwtType: String = "eu.europa.ec.eudi.cor.1"
+    override val sdJwtType: String = "urn:eu.europa.ec.eudi:cor:1"
+    override val isoNamespace: String = "eu.europa.ec.eudi.cor.1"
     override val supportedRepresentations: Collection<CredentialRepresentation> =
         listOf(SD_JWT)
+    override val claimNames: Collection<String> = CertificateOfResidenceDataElements.ALL_ELEMENTS
 }
